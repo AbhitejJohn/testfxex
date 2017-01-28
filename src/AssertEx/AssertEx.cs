672d9afc7a4ef83bfe83892e7b3756c851fd1858
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2017-01-29 01:36:50 +0530

    Assert Extensions.

--- src/AssertEx/AssertEx.cs
+++ src/AssertEx/AssertEx.cs
@@ -9,11 +9,11 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting.Extensions
     {
         /// <summary>
         /// Tests whether the code specified by delegate <paramref name="action"/> throws exact given exception of type <typeparamref name="T"/> (and not of derived type)
-        /// and throws 
+        /// with the specified message and throws 
         /// <code>
         /// AssertFailedException
         /// </code>
-        /// if code does not throws exception or throws exception of type other than <typeparamref name="T"/>.
+        /// if code does not throw exception that contains the specified message or throws exception of type other than <typeparamref name="T"/>.
         /// </summary>
         /// <param name="action">
         /// Delegate to code to be tested and which is expected to throw exception.
@@ -30,11 +30,40 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting.Extensions
         /// <returns>
         /// The type of exception expected to be thrown.
         /// </returns>
-        public static void ThrowsExceptionWithMessage<T>(Action action, string message) where T : Exception
+        public static T ThrowsExceptionWithMessage<T>(Action action, string message) where T : Exception
         {
             var exception = Assert.ThrowsException<T>(action);
 
             StringAssert.Contains(exception.Message, message);
+
+            return exception;
+        }
+
+        /// <summary>
+        /// Tests whether the code specified by delegate <paramref name="action"/> throws exact given exception of type <typeparamref name="T"/> (and not of derived type)
+        /// with the specified message and throws 
+        /// <code>
+        /// AssertFailedException
+        /// </code>
+        /// if code does not throw exception that contains the specified message or throws exception of type other than <typeparamref name="T"/>.
+        /// <param name="action">
+        /// Delegate to code to be tested and which is expected to throw exception.
+        /// </param>
+        /// <param name="message">
+        /// The message expected from the thrown exception.
+        /// </param>
+        /// <typeparam name="T">
+        /// Type of exception expected to be thrown.
+        /// </typeparam>
+        /// <exception cref="AssertFailedException">
+        /// Thrown if <paramref name="action"/> does not throws exception of type <typeparamref name="T"/>.
+        /// </exception>
+        /// <returns>
+        /// The type of exception expected to be thrown.
+        /// </returns>
+        public static T ThrowsExceptionWithMessage<T>(Func<object> action, string message) where T : Exception
+        {
+            return ThrowsExceptionWithMessage<T>(() => { action(); }, message);
         }
 
         /// <summary>
@@ -61,7 +90,36 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting.Extensions
         /// <returns>
         /// The type of exception expected to be thrown.
         /// </returns>
-        public static T ThrowsInnerException<T>(Action action, string message) where T : Exception
+        public static T ThrowsInnerException<T>(Func<object> action) where T : Exception
+        {
+            return ThrowsInnerException<T>(() => { action(); });
+        }
+
+        /// <summary>
+        /// Tests whether the code specified by delegate <paramref name="action"/> throws exact given exception/has inner exception of type <typeparamref name="T"/> (and not of derived type)
+        /// and throws.
+        /// <code>
+        /// AssertFailedException
+        /// </code>
+        /// if code does not throws exception or throws exception of type other than <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="action">
+        /// Delegate to code to be tested and which is expected to throw exception.
+        /// </param>
+        /// <param name="message">
+        /// The message to include in the exception when <paramref name="action"/>
+        /// does not throws exception of type <typeparamref name="T"/>.
+        /// </param>
+        /// <typeparam name="T">
+        /// Type of exception expected to be thrown.
+        /// </typeparam>
+        /// <exception cref="AssertFailedException">
+        /// Thrown if <paramref name="action"/> does not throws exception of type <typeparamref name="T"/>.
+        /// </exception>
+        /// <returns>
+        /// The type of exception expected to be thrown.
+        /// </returns>
+        public static T ThrowsInnerException<T>(Action action) where T : Exception
         {
             string finalMessage;
 
@@ -79,16 +137,20 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting.Extensions
                     var exType = innerException.GetType();
                     if (typeof(T).Equals(exType))
                     {
-                        return (T)ex;
+                        return (T)innerException;
                     }
+                    if(exTypeBuilder.Length != 0)
+                    {
+                        exTypeBuilder.Append(", ");
+                    }
+
                     exTypeBuilder.Append(exType);
-                    innerException = ex.InnerException;
+                    innerException = innerException.InnerException;
                 } while (innerException != null);
 
                 finalMessage = string.Format(
                         CultureInfo.CurrentCulture,
-                        "Could not find Exception of type {1} in exception chain {2}. {0} \r\n Exception Message {3} \r\n Stack Trace {4}",
-                        message ?? "(null)",
+                        "Could not find Exception of type {0} in exception chain {1}. \r\n Exception Message {2} \r\n Stack Trace {3}",
                         typeof(T).Name,
                     exTypeBuilder.ToString(),
                     ex.Message,
@@ -99,9 +161,9 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting.Extensions
 
             finalMessage = string.Format(
                 CultureInfo.CurrentCulture,
-                FrameworkMessages.NoExceptionThrown,
-                message ?? "(null)",
+                "No exception thrown. {0} exception was expected",
                 typeof(T).Name);
+
             Assert.Fail("Assert.ThrowsException {0}", finalMessage);
 
             // This will not hit, but need it for compiler.
